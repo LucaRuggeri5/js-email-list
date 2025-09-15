be9eee58a170a4a37304f77ff3eb1270f62b5918
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-15 17:03:17 +0200

    creazione in console delle 10 email

--- javascript/script.js
+++ javascript/script.js
@@ -3,18 +3,31 @@
 // generatore email API
 //  https://flynn.boolean.careers/exercises/api/random/mail
 
+const numbersList = document.getElementById("lista-email");
 const genera = document.getElementById("genera");
-
 
 const endPointApi = `https://flynn.boolean.careers/exercises/api/random/mail`;
 
-genera.addEventListener("click", emails);
+genera.addEventListener("click", emailCasuali);
 
-function emails() {
-    axios.get(endPointApi)
-        .then(rispApi => {
-            const result = rispApi.data.response;
-            console.log(result);
-        })
+function emailCasuali() {
+    numbersList.innerHTML = "";
+    for (let index = 0; index < 10; index++) {
+        axios.get(endPointApi)
+            .then(rispApi => {
+                const result = rispApi.data.response;
+                console.log(result);
 
-}
+                // svuoto il li e poi inserisco l'output
+                numbersList.innerHTML = "";
+                numbersList.innerHTML = result;
+
+                // faccio sì che mi si crei 10 volte nel li
+                const li = document.createElement("li");
+                li.textContent = result;
+                numbersList.appendChild(li);
+
+            })
+
+    }
+};
