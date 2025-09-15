fdcccb1af4c7b74f23e7137d6324024aad68b2e1
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-15 16:07:46 +0200

    creazione della mail con il tasto

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,18 @@
 // generatore email API
 //  https://flynn.boolean.careers/exercises/api/random/mail
 
+const genera = document.getElementById("genera");
+
+
+const endPointApi = `https://flynn.boolean.careers/exercises/api/random/mail`;
+
+genera.addEventListener("click", emails);
+
+function emails() {
+    axios.get(endPointApi)
+        .then(rispApi => {
+            const result = rispApi.data.response;
+            console.log(result);
+        })
+
+}
